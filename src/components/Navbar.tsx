b795726a4b4beeeda46aab9b53d02ab2b6f7a0ac
Divya Mishra <mishradivyagkp1999@gmail.com>
2024-03-30 10:48:27 +0530

    get serachvideos, update state of searchterm when input parameter changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,25 @@
 import React from "react"
-import { useAppSelector } from "../redux/Hooks/index.ts";
+import { useAppDispatch, useAppSelector } from "../redux/Hooks/index.ts";
+import { clearVideos, clearsearchTerm, searchTerms } from "../redux/reducers/index.ts";
+import { getsearchVideos } from "../redux/actioncreatos/getsearchpageVideos.ts";
 // import { searchTerm, searchTerms } from "../redux/reducers"
 
 
-// const handleSearch = (e) =>{
-//   e.prevent.default;
-   
-// }
+
  
 const Navbar = () => {
+  const dispatch = useAppDispatch();
   const searchTerm = useAppSelector((state) => state.youapp.searchTerm);
+  // const nextPageToken = useAppSelector((state) => state.youapp.nextPageToken);
+  const handleSubmit = () =>{
+    if(searchTerm!=null){
+      // dispatch(clearVideos())
+      dispatch(getsearchVideos())
+    }
+    else{
+      console.log("search")
+    }
+  }
   return (
     <>
     <div className =" wrapper">
@@ -20,13 +30,13 @@ const Navbar = () => {
       <i className="fa-brands fa-youtube"></i>
       <span>You Tube</span>
     </div>
-     <form >
+     <form  onSubmit={() =>handleSubmit()}>
       <div>
      <i className="fa-solid fa-magnifying-glass"></i>
      </div>
-      {/* <input type="text" value = {searchTerm}
-      onChange{}/> */}
-      <i className="fa-solid fa-xmark"></i>
+      <input type="text" value = {searchTerm}
+      onChange={(e)=>  dispatch(searchTerms(e.target.value))}/>
+      <i className="fa-solid fa-xmark" onClick={() => dispatch(clearsearchTerm())}></i>
       <i className="fa-solid fa-magnifying-glass"></i>
 
      </form>
