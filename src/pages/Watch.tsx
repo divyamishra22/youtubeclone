bbad0b7b06448ef203a666b5838a150382202c59
Divya Mishra <mishradivyagkp1999@gmail.com>
2024-03-30 16:59:11 +0530

    get video by id , get recommended videos

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../redux/Hooks';
+import { getVideo } from '../redux/actioncreatos/getvideoDetail';
 
 export const Watch = () => {
+  const {id} = useParams();
+  const dispatch = useAppDispatch();
+  const currentplaying = useAppSelector((state) => state.youapp.currentPlaying)
+
+  useEffect(() =>{
+    if(id){
+      dispatch(getVideo(id))
+    }
+  },[id, dispatch])
+
+  useEffect(() => {
+    if (currentplaying && id) dispatch(getRecommendedVideos(id));
+  }, [currentplaying, dispatch, id]);
+
   return (
     <div>Watch</div>
   )
 }
+function getRecommendedVideos(id: string): any {
+  throw new Error('Function not implemented.');
+}
+
